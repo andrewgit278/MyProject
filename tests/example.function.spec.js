bb043831ee91d536375350c934e1001243a92b14
Andrew Artemenko <andrewgit278@gmail.com>
2023-07-20 22:35:39 +0300

    Shit for Shit

---
+++ tests/example.function.spec.js
@@ -0,0 +1,41 @@
+const { errors } = require("@playwright/test");
+
+const age = 10;
+let projectname
+
+if(age>5){
+let b=5;
+console.log(a+b)
+
+} else{
+    Error('The problem')
+}
+
+const baseUrl = `https://${projectname}veleten.com`
+
+if(projectname == 'alpha'){
+
+  baseUrl= `https://${projectname}veleten.com`
+
+} else if (projectname == 'dev'){
+    baseUrl= `https://${projectname}veleten.com`
+
+} else{
+
+    return errors('wrong message')
+}
+
+// ? 
+
+let user;
+let adultAge;
+
+let accesAllowed = (adultAge>16)? true:false;
+
+let acessAge = prompt('Age', 16);
+
+let message = (acessAge<16)? 'Dont acess':
+(acessAge==16)? 'Acess':
+(acessAge>16)? 'Of corcse yes':
+'Of corcse yes'
+
